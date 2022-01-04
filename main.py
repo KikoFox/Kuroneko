7afcd5f4d6650a4472f385194d08b5a793d042b7
Rinat <darkssoulr@gmail.com>
2022-01-04 15:03:03 +0400

    alpha version v.0.1

--- main.py
+++ main.py
@@ -1,20 +1,17 @@
 import discord
 from discord.ext import commands
-import random
 from decouple import config
 import tools
+from fun import Fun
+from music import Music
 from PIL import Image
 from io import BytesIO
 
 
-description = '''An example bot to showcase the discord.ext.commands extension
-module.
-There are a number of utility commands being showcased here.'''
-
 intents = discord.Intents.default()
 intents.members = True
 
-bot = commands.Bot(command_prefix='Kuro ', description=description, intents=intents)
+bot = commands.Bot(command_prefix='Kuro ', intents=intents)
 
 
 @bot.event
@@ -44,62 +41,6 @@ async def on_member_join(member):
         await guild.system_channel.send(text, file=discord.File(f"img\profile_{name}.jpg"))
 
 
-@bot.command()
-async def add(ctx, left: int, right: int):
-    """Adds two numbers together."""
-    await ctx.send(left + right)
-
-
-@bot.command()
-async def roll(ctx, dice: str):
-    """Rolls a dice in NdN format."""
-    try:
-        rolls, limit = map(int, dice.split('-'))
-    except Exception:
-        await ctx.send('Format has to be in N-N!')
-        return
-
-    result = ', '.join(str(random.randint(1, limit)) for _ in range(rolls))
-    await ctx.send(result)
-
-
-@bot.command(description='For when you wanna settle the score some other way')
-async def choose(ctx, *choices: str):
-    """Chooses between multiple choices."""
-    await ctx.send(random.choice(choices))
-
-
-@bot.command()
-async def repeat(ctx, times: int, *, content='repeating...'):
-    """Repeats a message multiple times."""
-    for i in range(times):
-        await ctx.send(content)
-
-
-@bot.command()
-async def joined(ctx, member: discord.Member):
-    """Says when a member joined."""
-    await ctx.send('{0.name} joined in {0.joined_at}'.format(member))
-
-
-@bot.command()
-async def sleep(ctx):
-    await ctx.send('Z..z..z')
-
-
-@bot.group()
-async def cool(ctx):
-    """Says if a user is cool.
-    In reality this just checks if a subcommand is being invoked.
-    """
-    if ctx.invoked_subcommand is None:
-        await ctx.send('No, {0.subcommand_passed} is not cool'.format(ctx))
-
-
-@cool.command(name='bot')
-async def _bot(ctx):
-    """Is the bot cool?"""
-    await ctx.send('Yes, the bot is cool.')
-
-
+bot.add_cog(Music(bot))
+bot.add_cog(Fun(bot))
 bot.run(config('TOKEN'))
